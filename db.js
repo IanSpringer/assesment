9d3bec390577ebc63506991a5d205594a8dd4feb
Robert Springer <springerdrums@gmail.com>
2016-08-21 14:27:53 -0700

    hwfe

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 
-var mongo_uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/assesment';
+var mongo_uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/sb-assesment';
 
 // mongoose.Promise = global.Promise;
 mongoose.connect(mongo_uri, function(err){
