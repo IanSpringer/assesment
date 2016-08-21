0af5d809446c9ebb60a28123afa7095398f1f9f0
Robert Springer <springerdrums@gmail.com>
2016-08-21 11:54:22 -0700

    heroku test

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ var session      = require('express-session');
 
 var db = require('./db.js');
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
+
+app.use(allowCrossDomain)
 
 require('./passport')(passport); // pass passport for configuration
 
